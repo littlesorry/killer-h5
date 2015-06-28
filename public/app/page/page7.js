5036f126538101c2c6a489c0bcd78b2b60152897
littlesorry <chaumet.g@gmail.com>
2015-06-28 21:47:31 +0800

    update img

--- public/app/page/page7.js
+++ public/app/page/page7.js
@@ -1,6 +1,7 @@
 define(['jquery', 'progressbar'], function($, ProgressBar) {
 
 	var p7 = {
+		rendered : false
     };
 
     p7.render = function() {
@@ -11,41 +12,43 @@ define(['jquery', 'progressbar'], function($, ProgressBar) {
     			audioFade.play();
     		}, 1000);
 
-    		setTimeout(function() {
-				var startColor = '#ff57d0';
-				var endColor = '#ff57d0';
+    		if (!p7.renderer) {
+    			p7.renderer = true;
+				setTimeout(function() {
+					var startColor = '#ff57d0';
+					var endColor = '#ff57d0';
 
-				var element = document.getElementById('text-outer-1');
-				var circle = new ProgressBar.Circle(element, {
-				    color: "#ff57d0",
-				    trailColor: '#6a777d',
-				    trailWidth: 5,
-				    duration: 2500,
-				    easing: 'bounce',
-				    strokeWidth: 5
-				});
+					var element = document.getElementById('text-outer-1');
+					var circle = new ProgressBar.Circle(element, {
+					    color: "#ff57d0",
+					    trailColor: '#6a777d',
+					    trailWidth: 5,
+					    duration: 2500,
+					    easing: 'bounce',
+					    strokeWidth: 5
+					});
 
-				circle.animate(0.86);
-    		}, 1800);
+					circle.animate(0.86);
+	    		}, 1800);
 
+	    		setTimeout(function() {
+					var startColor = '#ff57d0';
+					var endColor = '#ff57d0';
 
-    		setTimeout(function() {
-				var startColor = '#ff57d0';
-				var endColor = '#ff57d0';
+					var element = document.getElementById('text-outer-r');
+					var circle = new ProgressBar.Circle(element, {
+					    color: "#ff57d0",
+					    trailColor: '#6a777d',
+					    trailWidth: 5,
+					    duration: 2500,
+					    easing: 'bounce',
+					    strokeWidth: 5
+					});
 
-				var element = document.getElementById('text-outer-r');
-				var circle = new ProgressBar.Circle(element, {
-				    color: "#ff57d0",
-				    trailColor: '#6a777d',
-				    trailWidth: 5,
-				    duration: 2500,
-				    easing: 'bounce',
-				    strokeWidth: 5
-				});
-
-				circle.animate(0.35);
-    		}, 2000);
-
+					circle.animate(0.35);
+	    		}, 2000);
+    		}
+    		
     	}, 1500);
     };
 

305fe30d497fa3f5fe7c3f307ce1964c429ef608
wangbaofeng112 <wangbaofeng112@pingan.com.cn>
2015-06-29 11:19:55 +0800

    update style

--- public/app/page/page8.js
+++ public/app/page/page8.js
@@ -2,5 +2,28 @@ define(['jquery', 'wechat'], function($, wechat) {
 
 	var p8 = {};
 
+	p8.share = function(idx) {
+		$(".page8 .overlay").show();
+		if (idx  == 1) {
+			$(".page .btn-outer-l").show();
+			wechat.shareTimeline("发现一款好无节操的陌生交友神器，你要不要約約看？");
+			wechat.shareFriend("发现一款好无节操的陌生交友神器，你要不要約約看？");
+		} else {
+			$(".page .btn-outer-r").show();
+			wechat.shareTimeline("说了半天無节操的交友神器呢！最後就给我看这个？");
+			wechat.shareFriend("说了半天無节操的交友神器呢！最後就给我看这个？");
+		}
+		audioBtn.play();
+		wechat.callback = function() {
+			$(".page8 .overlay").hide();
+		};
+	};
+
+	p8.shareClose = function() {
+		$(".page .btn-outer").hide();
+		$(".page8 .overlay").hide();
+		wechat.callback = null;
+	};
+	
 	return p8;
 });
